156e5338c003e2fc31a928ce8454185e8ebe2add
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-11-23 14:43:02 -0600

    Pulling files from server I will recode / use in project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-/*
- * Your Comment Header Here
- */
+/**************************************************************
+Name: Yolanda Gunter
+Assignment: Coding 08
+Purpose: Intro - jQuery and Boostrap
+Notes: Learning jQuery & Boostrap to use for my Final Project
+ **************************************************************/
 
 "use strict"
+        
+$(document).ready(function(){
+     // Replace text button
+    $("button").click(function(){
+        $("p").text("Hello World!");         
+    });
+    // Highlight element with id mark
+    $("#mark").css("background", "yellow");
 
+    // Highlight elements with class mark
+    $(".mark").css("background", "yellow");
+
+    // Highlight paragraph elements
+    $("p").css("background", "orange");
+    
+     // Highlight form field elements
+    $('input[type="text"]').css("background", "yellow");
+    $('input[type="password"]').css("background", "green");
+});
