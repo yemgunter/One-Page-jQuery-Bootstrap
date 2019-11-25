016f2993e617ed7dd22f452deaeb713c0759eb8d
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-11-24 22:03:00 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,27 +9,30 @@ Notes: Learning jQuery & Boostrap to use for my Final Project
         
 $(document).ready(function(){
     
-     // Menu element for navigation
-    $("navbar").css("background-color", "#740074");
-    
+   
     // Syle for Back To Top
-    $("#this-page-top").css("h5");
+    $('#this-page-top').css("h5");
     
     // Header element with id header
-    $("#header").css("text-align", "center");
+    $('#header').css("text-align", "center");
     
     // Heading paragraph elements
-    $("h1").css("text-align", "center");
-    $("h2").css("text-align", "left");
-    $("h3").css("text-align", "left");
-    $("h4").css("text-align", "left");
-    $("h5").css("text-align", "left");
-    $("h6").css("text-align", "left");
+    $('h1').css("text-align", "center");
+    $('h2').css("text-align", "left");
+    $('h3').css("text-align", "center");
+    $('h4').css("text-align", "left");
+    $('h5').css("text-align", "left");
+    $('h6').css("text-align", "left");
     
-     // Highlight form field elements
+    // About Image styling
+    $('.photo').css("verticle-align", "middle");
+    $('.photo').css("width", "200px");
+    
+     // Highlight form related styling and elements
+    $('#form-div').css("text-align", "center");
     $('input[type="text"]').css("background", "yellow");
     $('input[type="email"]').css("background", "yellow");
-    $('input[type="textarea"]').css("background", "yellow");
+    $('input[type="message"]').css("background", "yellow");
     
-    
+    $('.footer-section').css("background-color", "#e2d1dd");
 });
